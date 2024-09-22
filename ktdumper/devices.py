7c7fb5b18899487a49bee936da60659ab307e7dd
NaoyaShinota2004 <104295023+NaoyaShinota2004@users.noreply.github.com>
2024-09-22 01:33:43 -0700

    Add P-03C

--- ktdumper/devices.py
+++ ktdumper/devices.py
@@ -626,6 +626,12 @@ DEVICES = [
     }, exploit_flavor="C2", payload_base=0x83800000, usb_receive=0x80024768, usb_send=0x80024554,
        onenand_addr=0x0C000000),
 
+    Device("p-03c", 0x04da, 0x216b, {
+        "dump_nand": PiplOnenandDumper(),
+        "onenand_id": PiplOnenandId(),
+        "probe_onenand": PiplProbeOnenand(sweep_start=0x0),
+    }, exploit_flavor="C2", payload_base=0x83800000, onenand_addr=0x0C000000),
+
     Device("p-06c", 0x04da, 0x216b, {
         "dump_rom": PiplExploitMemoryDumper(base=0x0, size=0x8000),
         "dump_nand_peek_poke": PiplOnenandDumper(),
